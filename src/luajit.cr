76bdeef9cbe9027bef7d8236928e3b10b70a0195
Michael Wagner <mdwranger@gmail.com>
2023-07-20 08:52:57 -0400

    feat: wip bindings

--- src/luajit.cr
+++ src/luajit.cr
@@ -75,17 +75,47 @@ def luaL_dostring(l, str)
   r
 end
 
+macro lua_upvalueindex(i)
+  (LibLuajit::LUA_GLOBALSINDEX - {{i}})
+end
+
+LibLuajit.luaJIT_version_2_1_0_beta3
+
 l = LibLuajit.luaL_newstate
 LibLuajit.luaL_openlibs(l)
 
-lua_register(l, "basicCFunc", ->(state : LibLuajit::State*) : Int32 {
-  puts "This is my c function called from lua"
-  0
-})
+def basic_fn(num : Float64) : Tuple(Float64, Float64)
+  {num * num, num + 4.0}
+end
+
+def basic_c_fn(state : LibLuajit::State*) : Int32
+  num = LibLuajit.lua_tonumber(state, -1)
+  values = basic_fn(num)
+  values.each do |v|
+    LibLuajit.lua_pushnumber(state, v)
+  end
+  values.size
+end
+
+box = Box(typeof(->basic_c_fn(LibLuajit::State*))).box(->basic_c_fn(LibLuajit::State*))
+LibLuajit.lua_pushlightuserdata(l, box)
+LibLuajit.lua_pushcclosure(l, ->(state : LibLuajit::State*) : Int32 {
+  ptr = LibLuajit.lua_touserdata(state, lua_upvalueindex(1))
+  proc = Box(typeof(->basic_c_fn(LibLuajit::State*))).unbox(ptr)
+  proc.call(state)
+}, 1)
+lua_setglobal(l, "basicCFunc")
 
 luaL_dostring(l, <<-LUA)
 print("hello from lua")
-basicCFunc()
+local value1, value2 = basicCFunc(7)
+x = { name = "Michael" }
+print(value1, value2)
 LUA
+
+lua_getglobal(l, "x")
+LibLuajit.lua_getfield(l, -1, "name")
+name = String.new(LibLuajit.lua_tolstring(l, -1, nil))
+puts "name is #{name}"
 
 LibLuajit.lua_close(l)
