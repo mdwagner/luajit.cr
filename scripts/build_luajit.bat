28c90ecbab28d9a608856cee0e982ff668f2fa52
Michael Wagner <mdwranger@gmail.com>
2023-10-24 17:52:19 -0400

    wip: test gh action windows

--- scripts/build_luajit.bat
+++ scripts/build_luajit.bat
@@ -2,7 +2,7 @@
 SETLOCAL
 
 :: Define the destination path
-set DEST_PATH=ext\luajit
+set DEST_PATH=.\ext\luajit
 
 :: Create a unique temporary directory for the LuaJIT repository
 set LUAJIT_TEMP_DIR=%TEMP%\LuaJIT_Clone_%RANDOM%
@@ -15,19 +15,19 @@ if errorlevel 1 (
     echo Failed to clone the repository.
     exit /b 1
 )
-cd %LUAJIT_TEMP_DIR%
+cd %LUAJIT_TEMP_DIR%\src
 
 :: Set up the build environment (assuming Visual Studio's vcvarsall.bat)
 :: Adjust the path to vcvarsall.bat if needed
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-if errorlevel 1 (
-    echo Failed to set up the build environment.
-    exit /b 1
-)
+:: call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+:: if errorlevel 1 (
+::     echo Failed to set up the build environment.
+::     exit /b 1
+:: )
 
 :: Build LuaJIT
 echo Building LuaJIT...
-msvcbuild.bat
+.\msvcbuild.bat
 if errorlevel 1 (
     echo Failed to build LuaJIT.
     exit /b 1
@@ -38,10 +38,12 @@ if not exist %DEST_PATH% (
     mkdir %DEST_PATH%
 )
 
-:: Copy the generated .dll and .lib files to the destination path
-echo Copying .dll and .lib files to %DEST_PATH%...
-copy /Y src\*.dll %DEST_PATH%
-copy /Y src\*.lib %DEST_PATH%
+:: Copy the generated files to the destination path
+echo Copying files to %DEST_PATH%...
+copy /Y *.dll %DEST_PATH%
+copy /Y *.lib %DEST_PATH%
+copy /Y *.exp %DEST_PATH%
+copy /Y *.pdb %DEST_PATH%
 
 if errorlevel 1 (
     echo Failed to copy files.
