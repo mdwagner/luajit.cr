4c42637f36c33bda1a474ff05e76b9c99b26695d
Michael Wagner <mdwranger@gmail.com>
2023-11-23 11:12:25 -0500

    fix: specs are running again
    todo: write specs for wrapper functions

--- spec/luajit_spec.cr
+++ spec/luajit_spec.cr
@@ -1,9 +1,9 @@
 require "./spec_helper"
 
 describe Luajit do
-  it "works" do
-    Luajit.run do |state|
-      state.execute <<-LUA
+  it "works", tags: ["wip"] do
+    Luajit.once do |state|
+      state.execute! <<-LUA
       x = { name = "Michael" }
       LUA
 
@@ -17,41 +17,4 @@ describe Luajit do
       name.should eq("Michael")
     end
   end
-
-  #it "works2" do
-    #Luajit.run do |state|
-      #Sprite.lua_def(state)
-
-      #state.execute <<-LUA
-      #local sprite = Sprite.new()
-      #sprite:move(5, 7)
-      #sprite:draw()
-      #LUA
-    #end
-  #end
-
-  #it "Crystal API in Lua", tags: "api" do
-    #Luajit.run do |state|
-      #state.new_table
-      #table_index = state.size
-      #io = IO::Memory.new
-
-      #state.tap do |_state|
-        #_state.push("gen_secret_key")
-        #_state.push do |_|
-          #io.puts Random::Secure.base64(32)
-          #0
-        #end
-        #_state.set_table(table_index)
-      #end
-
-      #state.set_global("Lucky")
-
-      #state.execute <<-LUA
-      #Lucky.gen_secret_key()
-      #LUA
-
-      #io.to_s.should_not be_empty
-    #end
-  #end
 end
